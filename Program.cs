dacb6294013cf7c8e802dd104f168e502bdc0a62
vikinice <vikinice@yandex.ru>
2022-11-16 14:20:00 +0200

    функция вывода массива в консоль

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintArray(string[] array) // вывод массива в консоль
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
+    }
+    Console.WriteLine();
+}
